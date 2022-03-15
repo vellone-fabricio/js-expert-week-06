fdb79397e184880058e9b6d408e2452e3729b55d
Vellone <fabriciovellone@gmail.com>
2022-03-15 20:37:31 -0300

    middle first lesson

---
+++ backend/controller.js
@@ -0,0 +1,11 @@
+import { Service } from './service.js'
+
+export class Controller {
+  constructor() {
+    this.service = new Service();
+  }
+
+  async getFileStream(fileName) {
+    return this.service.getFileStream(fileName);
+  }
+}
